6a9ffe31039f840bbf875b4eec8446626390d472
Jose Farias <jose@37signals.com>
2024-10-22 10:45:32 -0600

    Remove unnecessary calls to `#id`

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,10 +1,10 @@
 module WorkflowsHelper
   def link_to_stage_picker(bubble, workflow)
-    link_to workflow.name, new_bucket_bubble_stage_picker_path(bubble.bucket, bubble, workflow_id: workflow.id)
+    link_to workflow.name, new_bucket_bubble_stage_picker_path(bubble.bucket, bubble, workflow_id: workflow)
   end
 
   def button_to_set_stage(bubble, stage)
-    button_to stage.name, bucket_bubble_stagings_path(bubble.bucket, bubble, stage_id: stage.id),
+    button_to stage.name, bucket_bubble_stagings_path(bubble.bucket, bubble, stage_id: stage),
       method: :post, class: [ "btn btn--small", { "fill-selected": stage == bubble.stage } ]
   end
 end
